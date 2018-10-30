1eb834f570a124037cc14a763da2c6fb3ae86f0c
Brandon Hoover <brandonlhoover@gmail.com>
2018-10-29 22:00:56 -0400

    Player Destroy
    Working on player destroy having some troubles will see if i can fix it by tonight just have some important stuff i dont want to lose right now

---
+++ Infinite runner/Assets/2D Space Shooter game/Scripts/PlayerDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDestroy : MonoBehaviour
+{
+    public GameObject Player;
+    public void OnTriggerEnter(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            Destroy(Player);
+    }
+}
