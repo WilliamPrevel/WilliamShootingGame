5347e77c365519fe205cfd290004bc4de8ce0ce1
Brandon Hoover <brandonlhoover@gmail.com>
2018-11-02 13:51:51 -0400

    Fixed most things from the break
    Made scene level2 main scene don't work on it please so we dont break it should mostly be fixed now

--- Infinite runner/Assets/2D Space Shooter game/Scripts/Enemy.cs
+++ Infinite runner/Assets/2D Space Shooter game/Scripts/Enemy.cs
@@ -2,36 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
-
+public class Enemy : MonoBehaviour
+{
     public static int player_score = 0;
     public int Health = 100;
-    public GameObject destroyPoint;
-
-    public void Update()
+   
+    public void TakeDamage(int damage)
     {
+        Health -= damage;
+
         if (Health <= 0)
         {
             Die();
         }
-        else if (transform.position.y <= destroyPoint.transform.position.y)
-        {
-            //Destroy(gameObject);
-            Health = 0;
-            gameObject.SetActive(false);
-
-        }
     }
 
-    public void TakeDamage (int damage)
-    {
-        Health -= damage;
-    }
-
-     void Die()
+    void Die()
     {
         player_score += 15;
-        gameObject.SetActive(false);
-        //Destroy(gameObject);
+        Destroy(gameObject);
     }
 }
