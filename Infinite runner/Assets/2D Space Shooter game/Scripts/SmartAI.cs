d108919a9932a37a19efeb54eb2fada8fd22059d
Brandon Hoover <brandonlhoover@gmail.com>
2018-11-02 12:56:29 -0400

    made a smart AI
    Made a smart AI unit that tracks you shoots at you and will go closer to you and back away from you

---
+++ Infinite runner/Assets/2D Space Shooter game/Scripts/SmartAI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartAI : MonoBehaviour
+{
+    public float speed;
+    public float StopDistance;
+    public float LeaveDistance;
+
+    private float ShotTimer;
+    public float ShotTimer2;
+
+    public GameObject Shots;
+    private Transform Ship;
+	// Use this for initialization
+	void Start ()
+    {
+        Ship = GameObject.FindGameObjectWithTag("Player").transform;
+        ShotTimer = ShotTimer2;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Vector2.Distance(transform.position, Ship.position) > StopDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Ship.position, speed * Time.deltaTime);
+        }
+        else if(Vector2.Distance(transform.position, Ship.position) < StopDistance && Vector2.Distance(transform.position, Ship.position) > LeaveDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        
+        if(ShotTimer <= 0)
+        {
+            Instantiate(Shots, transform.position, Quaternion.identity);
+            ShotTimer = ShotTimer2;
+        }
+        else
+        {
+            ShotTimer -= Time.deltaTime;
+        }
+	}
+}
