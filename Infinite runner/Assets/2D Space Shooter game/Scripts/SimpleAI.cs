ae3d4d6ea88266c0419a478c16f90e1ec252151b
William <wprevel9@gmail.com>
2018-11-04 17:52:21 -0500

    Made simple AI
    The big enemy will follow the direction of the player at half speed

---
+++ Infinite runner/Assets/2D Space Shooter game/Scripts/SimpleAI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAI : MonoBehaviour {
+
+    public float speed;
+    public float StopDistance;
+    public float LeaveDistance;
+
+    private Transform Ship;
+    // Use this for initialization
+    void Start()
+    {
+        Ship = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            transform.position = Vector2.MoveTowards(transform.position, (Ship.position/2), speed * Time.deltaTime);
+
+    }
+}
