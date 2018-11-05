aa6f4896777519c81fa842c9f3850c05eb18f06e
Brandon Hoover <brandonlhoover@gmail.com>
2018-11-04 21:17:49 -0500

    Last attempt
    Tried to fix the homing laser couldn't do it. tried to add collision for the player but that negates the shooting on the player so reverted that could not make another AI was too stressed after trying to add in the rest just have to hand it in not much help i was getting with a 2 person job for the amount of time.

---
+++ Infinite runner/Assets/missile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class missile : MonoBehaviour
+{
+    public Transform LockOn;
+    public float Speed = 5f;
+    public float rotateSpeed = 190f;
+    private Rigidbody2D RB;
+	// Use this for initialization
+	void Start ()
+    {
+      RB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
+        Vector2 direction = (Vector2)LockOn.position - RB.position;
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+        RB.angularVelocity = -rotateAmount * rotateSpeed;
+        RB.velocity = transform.up * Speed;
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
